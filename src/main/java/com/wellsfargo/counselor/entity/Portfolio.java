89c141d441c93039cded181996c0034f509a7069
SATHISHA G R <sathishagr4@gmail.com>
2026-06-21 21:06:44 +0530

    "Commit changes"

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,53 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+
+    @Column(nullable = false)
+    private String name;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, String name) {
+        this.client = client;
+        this.name = name;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
